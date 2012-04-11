412f85df2638e9854e0dcbcc63d4c4ac3fea0b9b
Eric Belanger <ebelanger@360-innovations.com>
2012-04-11 11:26:37 -0400

    added some documentation.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/server/core/ComponentPropertyPainter.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/server/core/ComponentPropertyPainter.java
@@ -7,26 +7,55 @@ import com.vaadin.terminal.PaintException;
 import com.vaadin.terminal.PaintTarget;
 import com.vaadin.ui.Component;
 
+/**
+ * Server side class that manages painting of component references and lists. Application attachment of components is managed internally, so it is
+ * not necessary to update those states manually.
+ * 
+ * PaintablePropertyUpdater is the client-side counterpart to this class.
+ */
 public class ComponentPropertyPainter {
 	private final Set<ComponentProperty> properties = Sets.newLinkedHashSet();
 	private final Component parent;
 
+	/**
+	 * Instantiate a new ComponentPropertyPainter linked to the specified parent component.
+	 * 
+	 * @param parent the parent component to be used to manage application attachment.
+	 */
 	public ComponentPropertyPainter(Component parent) {
 		this.parent = parent;
 	}
 
+	/**
+	 * Creates a new component reference that will be managed on paint.
+	 * 
+	 * @param propertyName the unique registration property name.  the same name must be used on the client-side.
+	 * @return the registered reference.
+	 */
 	public <T extends Component> ComponentReference<T> addProperty(String propertyName) {
 		final ComponentReference<T> reference = new ComponentReference<T>(parent, "$" + propertyName);
 		properties.add(reference);
 		return reference;
 	}
 
+	/**
+	 * Creates a new component list that will be managed on paint.
+	 * 
+	 * @param propertyName the unique registration property name.  the same name must be used on the client-side.
+	 * @return the registered component list.
+	 */
 	public <T extends Component> ComponentList<T> addComponentList(String propertyName) {
 		final ComponentList<T> components = new ComponentList<T>(parent, "$" + propertyName);
 		properties.add(components);
 		return components;
 	}
 
+	/**
+	 * Equivalent to Paintable.paint(PaintTarget) that must be called when painting the linked parent component.
+	 * 
+	 * @param target the parent component target.
+	 * @throws PaintException when a exception occurs during painting.
+	 */
 	public void paintContent(PaintTarget target) throws PaintException {
 		for (ComponentProperty property : properties) {
 			property.paintContent(target);
