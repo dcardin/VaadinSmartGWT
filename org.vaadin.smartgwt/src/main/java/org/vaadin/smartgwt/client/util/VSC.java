e7bd3df90a1697806fbd7a114325f3ba78e03409
Eric Belanger <ebelanger@360-innovations.com>
2012-04-12 13:57:16 -0400

    code cleanup and format.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/util/VSC.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/util/VSC.java
@@ -13,71 +13,52 @@ import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
 
-public class VSC extends Widget implements Paintable, ClientSideHandler
-{
+public class VSC extends Widget implements Paintable, ClientSideHandler {
 	private final Element element = DOM.createDiv();
 	protected String paintableId;
 	private ApplicationConnection client;
 	private final ClientSideProxy rpc = new ClientSideProxy("VSC", this);
 
-	public VSC()
-	{
-		rpc.register("sayNoCallback", new Method()
-			{
-				public void invoke(final String methodName, final Object[] data)
-				{
-					if (data.length == 1)
-					{
-						SC.say((String) data[0]);
-					}
-					else
-					{
-						SC.say((String) data[0], (String) data[1]);
-					}
+	public VSC() {
+		rpc.register("sayNoCallback", new Method() {
+			public void invoke(final String methodName, final Object[] data) {
+				if (data.length == 1) {
+					SC.say((String) data[0]);
+				} else {
+					SC.say((String) data[0], (String) data[1]);
 				}
-			});
+			}
+		});
 
-		rpc.register("sayWithCallback", new Method()
-			{
-				public void invoke(final String methodName, final Object[] data)
-				{
-					if (data.length == 1)
-					{
-						// simple message
-						SC.say((String) data[0], new BooleanCallback()
-							{
-								@Override
-								public void execute(Boolean value)
-								{
-									client.updateVariable(paintableId, "callback", value != null ? value : false, true);
-								}
-							});
-					}
-					else
-					{
-						SC.say((String) data[0], (String) data[1], new BooleanCallback()
-							{
-								@Override
-								public void execute(Boolean value)
-								{
-									client.updateVariable(paintableId, "callback", value != null ? value : false, true);
-								}
-							});
-					}
+		rpc.register("sayWithCallback", new Method() {
+			public void invoke(final String methodName, final Object[] data) {
+				if (data.length == 1) {
+					// simple message
+					SC.say((String) data[0], new BooleanCallback() {
+						@Override
+						public void execute(Boolean value) {
+							client.updateVariable(paintableId, "callback", value != null ? value : false, true);
+						}
+					});
+				} else {
+					SC.say((String) data[0], (String) data[1], new BooleanCallback() {
+						@Override
+						public void execute(Boolean value) {
+							client.updateVariable(paintableId, "callback", value != null ? value : false, true);
+						}
+					});
 				}
-			});
+			}
+		});
 	}
 
-
 	@Override
-	public Element getElement()
-	{
+	public Element getElement() {
 		return element;
 	}
 
 	@Override
-	public void updateFromUIDL(UIDL uidl, ApplicationConnection client)
-	{
+	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
 		rpc.update(this, uidl, client);
 
 		this.client = client;
@@ -85,15 +66,13 @@ public class VSC extends Widget implements Paintable, ClientSideHandler
 	}
 
 	@Override
-	public boolean initWidget(Object[] params)
-	{
+	public boolean initWidget(Object[] params) {
 		rpc.clientInitComplete();
 		return true;
 	}
 
 	@Override
-	public void handleCallFromServer(String method, Object[] params)
-	{
+	public void handleCallFromServer(String method, Object[] params) {
 		System.out.println("method call: " + method);
 	}
 
