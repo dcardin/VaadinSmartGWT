81d59c6819a7ce797042e307b48a5a5da68d594b
Eric Belanger <ebelanger@360-innovations.com>
2012-04-13 16:11:56 -0400

    fixed displayed error field when validation was null.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/form/fields/VFormItem.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/form/fields/VFormItem.java
@@ -1,43 +1,35 @@
 package org.vaadin.smartgwt.client.ui.form.fields;
 
+import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.fields.FormItem;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.UIDL;
 
-public class VFormItem extends VAbstractFormItem<FormItem, String>
-{
-	public VFormItem()
-	{
+public class VFormItem extends VAbstractFormItem<FormItem, String> {
+	public VFormItem() {
 		super(new FormItem());
 	}
 
 	@Override
-	protected String getUIDLFormItemValue(UIDL uidl, String attributeName)
-	{
+	protected String getUIDLFormItemValue(UIDL uidl, String attributeName) {
 		return uidl.getStringAttribute(attributeName);
 	}
 
 	@Override
-	protected String getFormItemValue()
-	{
+	protected String getFormItemValue() {
 		return getJSObject().getValue() == null ? null : getJSObject().getValue().toString();
 	}
 
 	@Override
-	protected void postAttributeUpdateFromUIDL(UIDL uidl, ApplicationConnection client)
-	{
-		if (uidl.hasAttribute("*errorMessages"))
-		{
-			if (getJSObject().getForm() != null)
-			{
-				getJSObject().getForm().setFieldErrors(getJSObject().getName(), uidl.getStringArrayAttribute("*errorMessages"), true);
-			}
-		}
-		else
-		{
-			if (getJSObject().getForm() != null)
-			{
-				getJSObject().getForm().clearFieldErrors(getJSObject().getName(), true);
+	protected void postAttributeUpdateFromUIDL(UIDL uidl, ApplicationConnection client) {
+		final String name = getJSObject().getName();
+		final DynamicForm form = getJSObject().getForm();
+
+		if (form != null) {
+			if (uidl.hasAttribute("*errorMessages") && "null".equals(uidl.getStringArrayAttribute("*errorMessages"))) {
+				form.setFieldErrors(name, uidl.getStringArrayAttribute("*errorMessages"), true);
+			} else {
+				form.clearFieldErrors(name, true);
 			}
 		}
 	}
