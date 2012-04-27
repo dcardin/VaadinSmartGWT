a30a4992dde5e3b0eca0544f90f29eb880118709
Eric Belanger <ebelanger@360-innovations.com>
2012-04-27 09:35:51 -0400

    mark window for destruction after being closed since it will be destroyed on the server.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/VWindow.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/VWindow.java
@@ -4,10 +4,12 @@ import org.vaadin.smartgwt.client.core.PaintableListListener;
 import org.vaadin.smartgwt.client.core.PaintablePropertyUpdater;
 import org.vaadin.smartgwt.client.ui.utils.PainterHelper;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.Window;
+import com.smartgwt.client.widgets.events.CloseClickHandler;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
@@ -44,6 +46,13 @@ public class VWindow extends Window implements Paintable {
 				removeItem((Canvas) element);
 			}
 		});
+	}
+
+	@Override
+	public HandlerRegistration addCloseClickHandler(CloseClickHandler handler) {
+		final HandlerRegistration registration = super.addCloseClickHandler(handler);
+		markForDestroy();
+		return registration;
 	}
 
 	@Override
