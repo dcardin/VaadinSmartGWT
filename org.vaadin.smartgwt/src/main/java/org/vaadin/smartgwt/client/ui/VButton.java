bbe21a0b78fdcf97f67d2ec6cfe470fc7233487c
Eric Belanger <ebelanger@360-innovations.com>
2012-05-29 17:08:28 -0400

    added client side button click handler registration.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/VButton.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/VButton.java
@@ -1,51 +1,49 @@
 package org.vaadin.smartgwt.client.ui;
 
+import org.vaadin.smartgwt.client.core.ServerSideEventRegistration;
 import org.vaadin.smartgwt.client.ui.utils.PainterHelper;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.smartgwt.client.widgets.Button;
+import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
 
-public class VButton extends Button implements Paintable
-{
-	public static final String CLICK_EVENT_IDENTIFIER = "click";
-
+public class VButton extends Button implements Paintable {
 	private final Element element = DOM.createDiv();
-	protected String paintableId;
-	protected ApplicationConnection client;
+	private String pid;
+	private ApplicationConnection client;
+	private ServerSideEventRegistration clickHandlerEventRegistration;
 
 	@Override
-	public Element getElement()
-	{
+	public Element getElement() {
 		return element;
 	}
 
-	public VButton()
-	{
-		super();
-
-		addClickHandler(new ClickHandler()
-			{
-				@Override
-				public void onClick(com.smartgwt.client.widgets.events.ClickEvent event)
-				{
-					String button = "left click";
-					client.updateVariable(paintableId, CLICK_EVENT_IDENTIFIER, button, true);
-				}
-			});
-	}
-
 	@Override
-	public void updateFromUIDL(UIDL uidl, ApplicationConnection client)
-	{
-		this.client = client;
-		paintableId = uidl.getId();
+	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+		if (this.pid == null) {
+			this.pid = uidl.getId();
+			this.client = client;
+
+			clickHandlerEventRegistration = new ServerSideEventRegistration("*hasClickHandlers") {
+				@Override
+				protected HandlerRegistration registerHandler() {
+					return addClickHandler(new ClickHandler() {
+						@Override
+						public void onClick(ClickEvent event) {
+							VButton.this.client.updateVariable(pid, "clickEvent", true, true);
+						}
+					});
+				}
+			};
+		}
 
 		PainterHelper.updateSmartGWTComponent(client, this, uidl);
+		clickHandlerEventRegistration.updateFromUIDL(uidl);
 	}
-
 }
