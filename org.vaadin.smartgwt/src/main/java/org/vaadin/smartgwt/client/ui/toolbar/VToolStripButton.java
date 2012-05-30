319815570af6fa434e74c20cf4720bf1280d1603
Eric Belanger <ebelanger@360-innovations.com>
2012-05-30 08:14:01 -0400

    added client side click handling to ToolStripButton.

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/toolbar/VToolStripButton.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/ui/toolbar/VToolStripButton.java
@@ -1,51 +1,53 @@
 package org.vaadin.smartgwt.client.ui.toolbar;
 
+import org.vaadin.smartgwt.client.core.ServerSideEventRegistration;
 import org.vaadin.smartgwt.client.ui.utils.PainterHelper;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
+import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.toolbar.ToolStripButton;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.Paintable;
 import com.vaadin.terminal.gwt.client.UIDL;
 
-public class VToolStripButton extends ToolStripButton implements Paintable
-{
-	public static final String CLICK_EVENT_IDENTIFIER = "click";
-
+public class VToolStripButton extends ToolStripButton implements Paintable {
 	private final Element element = DOM.createDiv();
-	protected String paintableId;
-	protected ApplicationConnection client;
+	private String pid;
+	private ApplicationConnection client;
+	private ServerSideEventRegistration clickHandlerEventRegistration;
 
-	@Override
-	public Element getElement()
-	{
-		return element;
-	}
-
-	public VToolStripButton()
-	{
+	public VToolStripButton() {
 		super();
-
-		addClickHandler(new ClickHandler()
-			{
-				@Override
-				public void onClick(com.smartgwt.client.widgets.events.ClickEvent event)
-				{
-					String button = "left click";
-					client.updateVariable(paintableId, CLICK_EVENT_IDENTIFIER, button, true);
-				}
-			});
 	}
 
 	@Override
-	public void updateFromUIDL(UIDL uidl, ApplicationConnection client)
-	{
-		this.client = client;
-		paintableId = uidl.getId();
+	public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+		if (pid == null) {
+			this.pid = uidl.getId();
+			this.client = client;
 
+			clickHandlerEventRegistration = new ServerSideEventRegistration("*hasClickHandlers") {
+				@Override
+				protected HandlerRegistration registerHandler() {
+					return addClickHandler(new ClickHandler() {
+						@Override
+						public void onClick(ClickEvent event) {
+							VToolStripButton.this.client.updateVariable(pid, "clickEvent", true, true);
+						}
+					});
+				}
+			};
+		}
+
+		clickHandlerEventRegistration.updateFromUIDL(uidl);
 		PainterHelper.updateSmartGWTComponent(client, this, uidl);
 	}
 
+	@Override
+	public Element getElement() {
+		return element;
+	}
 }
