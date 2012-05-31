e096058cf7cd110c52c4f5aa77dd416fbe0a715b
Daniel Cardin <Daniel.Cardin@360-innovations.com>
2012-05-31 12:07:08 -0400

    New smooth image handling

--- org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/VRendererPanel.java
+++ org.vaadin.smartgwt/src/main/java/org/vaadin/smartgwt/client/VRendererPanel.java
@@ -7,8 +7,13 @@ import org.vaadin.rpc.client.ClientSideProxy;
 import org.vaadin.smartgwt.client.ui.utils.PainterHelper;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.LoadEvent;
+import com.google.gwt.event.dom.client.LoadHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
-import com.smartgwt.client.widgets.Canvas;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.types.AnimationEffect;
+import com.smartgwt.client.widgets.AnimationCallback;
 import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.events.ResizedEvent;
 import com.smartgwt.client.widgets.events.ResizedHandler;
@@ -19,23 +24,49 @@ import com.vaadin.terminal.gwt.client.UIDL;
 
 public class VRendererPanel extends VLayout implements Paintable {
 	private Img main;
-	private boolean onMain = true;
 
 	private final ClientSideProxy rpc = new ClientSideProxy("VRendererPanel", new ClientSideHandlerImpl());
 	private String pid;
-
+	private Image img = new Image();
+	private String url;
+	private int height;
+	private int width;
+	
 	public VRendererPanel() {
 		main = new Img();
 		main.setHeight("99%");
 		main.setWidth("99%");
+		main.setAnimateShowTime(1000);
+		main.setAnimateHideTime(1000);
 		
 		addMember(main);
 
 		addResizedHandler(new ResizedHandler() {
 			@Override
 			public void onResized(ResizedEvent event) {
-				refresh();
+				if (getWidth() != width || getHeight() != height)
+				{
+					height = getHeight();
+					width = getWidth();
+					refresh();
+				}
 			}
+		});
+		
+		img.setVisible(false);
+	    RootPanel.get().add(img);
+	    
+		img.addLoadHandler(new LoadHandler() {
+			@Override
+			public void onLoad(LoadEvent event) {
+				main.animateHide(AnimationEffect.FADE, new AnimationCallback() {
+					@Override
+					public void execute(boolean earlyFinish) {
+						main.setSrc(url);
+						main.animateShow(AnimationEffect.FADE);
+					}
+				});
+			};
 		});
 	}
 
@@ -55,8 +86,11 @@ public class VRendererPanel extends VLayout implements Paintable {
 
 		DateTimeFormat format = DateTimeFormat.getFormat("MMddyyyyHHmmssS");
 		String date = format.format(new Date());
+		url = GWT.getHostPageBaseURL() + "imageFetcher?type=main&width=" + width + "&height=" + height + "&time=" + date;
 
-		main.setBackgroundImage(GWT.getHostPageBaseURL() + "imageFetcher?type=main&width=" + width + "&height=" + height + "&time=" + date);
+		img.setHeight(height);
+		img.setWidth(width);
+		img.setUrl(url);
 	}
 
 	private class ClientSideHandlerImpl implements ClientSideHandler {
